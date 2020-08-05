cf8955d451d9f73589d006659b8ef4bcddcf21e8
David Salcedo <dvsalcedov@gmail.com>
2020-08-04 19:51:51 -0500

    Working project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ReplaceEmptySpace
+{
+    class Program
+    {
+        const String CONSCHAR = "%20";
+        static void Main(string[] args)
+        {
+            replaceChar("Hello World!");
+        }
+
+        static void replaceChar(string sentence){
+            char [] chars = sentence.ToCharArray();
+            StringBuilder sb = new StringBuilder(); 
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == (char) 32) {
+                    sb.Append(CONSCHAR);
+                }else{
+                    sb.Append(chars[i]);
+                }
+            }
+
+            System.Console.WriteLine(sb.ToString());
+        }
+    }
+}
